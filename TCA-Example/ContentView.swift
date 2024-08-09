b405c679408b2befbbc153304a740910d1f82d68
신승욱 <ukseung.dev@incross.com>
2024-08-09 11:05:14 +0900

    Initial Commit

---
+++ TCA-Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-Example
+//
+//  Created by ukseung.dev on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-Example/TCA_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ExampleApp.swift
+//  TCA-Example
+//
+//  Created by ukseung.dev on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
